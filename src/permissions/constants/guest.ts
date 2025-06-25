2b8819496cab3ed02b0bdec901d9d7436845bb57
MinJi Cho <jam96013@gmail.com>
2025-06-25 16:59:26 +0900

    [chore]

--- src/permissions/constants/guest.ts
+++ src/permissions/constants/guest.ts
@@ -1,4 +1,4 @@
 export const GUEST_ACCOUNTS = {
-  email: process.env.NEXT_PUBLIC_GUEST_EMAIL ?? "",
-  password: process.env.NEXT_PUBLIC_GUEST_PASSWORD ?? "",
+  email: "guest@gmail.com",
+  password: "qwer1155",
 };
